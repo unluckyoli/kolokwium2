30b1075905600e680d185974c039c0aaa7f4270e
Olivier Kopala <s28526@pjwstk.edu.pl>
2024-06-11 11:42:02 +0200

    bazka

---
+++ kolokwiumDrugie/kolokwiumDrugie/Migrations/20240611093054_Init.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace kolokwiumDrugie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    CurrentWeight = table.Column<int>(type: "int", nullable: false),
+                    MaxWeight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_characters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_items", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "titles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_titles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "backpacks",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(type: "int", nullable: false),
+                    ItemId = table.Column<int>(type: "int", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_backpacks", x => new { x.CharacterId, x.ItemId });
+                    table.ForeignKey(
+                        name: "FK_backpacks_characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_backpacks_items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "items",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "character_titles",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(type: "int", nullable: false),
+                    TitleId = table.Column<int>(type: "int", nullable: false),
+                    AcquiredAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character_titles", x => new { x.CharacterId, x.TitleId });
+                    table.ForeignKey(
+                        name: "FK_character_titles_characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "characters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_character_titles_titles_TitleId",
+                        column: x => x.TitleId,
+                        principalTable: "titles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "characters",
+                columns: new[] { "Id", "CurrentWeight", "FirstName", "LastName", "MaxWeight" },
+                values: new object[,]
+                {
+                    { 1, 43, "John", "Kowalski", 200 },
+                    { 2, 70, "Leo", "Messi", 175 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "items",
+                columns: new[] { "Id", "Name", "Weight" },
+                values: new object[,]
+                {
+                    { 1, "widelec", 10 },
+                    { 2, "marchew", 11 },
+                    { 3, "noga", 12 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "titles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Title1" },
+                    { 2, "Title2" },
+                    { 3, "Title3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "backpacks",
+                columns: new[] { "CharacterId", "ItemId", "Amount" },
+                values: new object[,]
+                {
+                    { 1, 1, 2 },
+                    { 1, 3, 4 },
+                    { 2, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "character_titles",
+                columns: new[] { "CharacterId", "TitleId", "AcquiredAt" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 6, 10, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 1, 2, new DateTime(2024, 6, 9, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 1, 3, new DateTime(2024, 6, 8, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_backpacks_ItemId",
+                table: "backpacks",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_character_titles_TitleId",
+                table: "character_titles",
+                column: "TitleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "backpacks");
+
+            migrationBuilder.DropTable(
+                name: "character_titles");
+
+            migrationBuilder.DropTable(
+                name: "items");
+
+            migrationBuilder.DropTable(
+                name: "characters");
+
+            migrationBuilder.DropTable(
+                name: "titles");
+        }
+    }
+}
